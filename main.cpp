778e3a0bdfb853ffae685c9e307977d0c188d25a
Wizer27 <svd2yar@gmail.com>
2025-06-05 13:39:39 +0300

    Add a bin function

--- main.cpp
+++ main.cpp
@@ -2,10 +2,20 @@
 #include <algorithm>
 #include <vector>
 #include <string>
-
+#include <math.h>
+#include <bitset>
 
 using namespace std;
 
+
+
+string str_to_bin(string word){
+	string bin_out;
+	for(char i : word){
+		bin_out += std::bitset<8>(i).to_string() + " ";
+	}
+	return bin_out;
+}
 void print(string name){
 	std::cout << name << endl;
 }
@@ -14,8 +24,12 @@ void print(int name){
 	std::cout << name << endl;
 }
 
+
+
+
 int main(){
 	print("Test");
 	print(10);
+	print(str_to_bin("AB"));
 	return 0;
 }
