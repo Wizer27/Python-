a8d08d0b7c635b982dbfc0faee4c504db89ba221
Wizer27 <svd2yar@gmail.com>
2025-06-07 13:24:43 +0300

    still trying

--- main.cpp
+++ main.cpp
@@ -31,7 +31,7 @@ void print(int name){
 	std::cout << name << endl;
 }
 
-void sha256v(){
+/*void sha256v(){
 	const int k = 31,mod = 1e9+7;
 	string s = "ajdfkljfklsdf";
 	long long h = 0, m = 1;
@@ -40,15 +40,35 @@ void sha256v(){
 	    h = (h + m * x) % mod;
 	    m = (m * k) % mod;
 	}
-	std::cout << m << endl;  
+	const int maxn = 1e5+5;
+	int p[maxn];
+	p[0] = 1;
+	for (int i = 1; i < maxn; i++){
+		p[i] = (p[i-1] * k) % mod;
+	}
+    //double  h[maxn];
+	h[0] = 0; 
+
+
+
+	for (int i = 0; i < n; i++) 
+    	h[i+1] = (h[i] + p[i] * s[i]) % mod;
+
+	//std::cout << m << endl; 
+	//std::cout << (h[r+1] - h[l]) * p[n-l] % mod << endl;
+	std::cout << h << endl;
+
 }
-
-
+*/
+unsigned long  hash_test(const string str){
+	unsigned long hash = 5381;  // начальное значение
+    for (char c : str) {
+      hash = ((hash << 5) + hash) + c;  // умножение на 33
+    }
+    return hash;
+}
 //main - is a demo function 
 int main(){
-	print("Test");
-	print(10);
-	print(str_to_bin("hello word"));
-	print(len("test"));
+	std::cout << hash_test("testr") << endl;
 	return 0;
 }
